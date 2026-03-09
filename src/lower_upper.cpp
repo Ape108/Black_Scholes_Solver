b01764ac3b9eebde1a24b15cfb394784787d303d
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2026-03-09 16:46:47 +0000

    Working Lower Upper Decomposition

--- src/lower_upper.cpp
+++ src/lower_upper.cpp
@@ -5,11 +5,12 @@ Decomposed Decomp(const std::vector<float>& a, const std::vector<float>& b, cons
     LU.upper.push_back(a[0]); // Set u_1 <--- a_1
     std::size_t n = a.size();
 
-    for (std::size_t i=1; i<=n-1; i++) {
-        float l_i = c[i] / LU.upper[i-1]; // l_i = c_i / u_i-1
+    for (std::size_t i=1; i<n; i++) {
+        float l_i = c[i-1] / LU.upper[i-1]; // l_i = c_i / u_i-1
         LU.lower.push_back(l_i);
         float u_i = a[i] - (l_i * b[i-1]);
         LU.upper.push_back(u_i); 
     }
 
+    return LU;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,12 @@ int main() {
     std::vector<float> c = {2, 3};
 
     Decomposed LU = Decomp(a, b, c);
+
+    std::cout << "Expected: {2.0, 0.6}" << std::endl;
     std::copy(LU.lower.begin(), LU.lower.end(), std::ostream_iterator<float>(std::cout, " "));
+    std::cout << std::endl;
+    std::cout << "Expected: {1.0, 5.0, 0.2}" << std::endl;
     std::copy(LU.upper.begin(), LU.upper.end(), std::ostream_iterator<float>(std::cout, " "));
-};
+    std::cout << std::endl;
+    return 0;
+}
