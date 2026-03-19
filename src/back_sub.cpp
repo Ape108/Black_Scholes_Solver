754c612ca52a028f62f6d39c06ba59e70482fa9b
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2026-03-19 01:58:44 +0000

    Backsub logic

--- src/back_sub.cpp
+++ src/back_sub.cpp
@@ -1,16 +1,28 @@
 #include "../include/back_sub.hpp"
 
+#include <stack>
+
 std::vector<float> backward_substitution(const std::vector<float>& u, const std::vector<float>& b, const std::vector<float>& y) {
 
     std::size_t n = u.size();
-    std::vector<float> x = {static_cast<float>(y[n]) / u[n]};
+    std::stack<float> x;
+
+    x.push(y[n] / u[n]);
     
     for (std::size_t i=n-1; i >= 0; i--) {
         float x_i = y[i] - (b[i+1] * y[i+1]);
-        // x.push_front() DONT PUSH FRONT because it's O(n) to shift everything. 
-        // Ill use a stack then pop everything from the stack into the vector result
-        // at the end.
+        x_i = x_i / u[i];
+        x.push(x_i);
     }
 
-    return x;
+    std::vector<float> solution;
+
+    // Pop all values from stack into the vector in correct order
+    while (!x.empty()) {
+        float top = x.top();
+        solution.push_back(top);
+        x.pop();
+    }
+
+    return solution;
 }
