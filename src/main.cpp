fa769bf0036cad2862301192938677e18bbc73e1
Cameron Akhtar <cameronakhtar.kc@gmail.com>
2026-03-21 00:36:20 +0000

    Refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,32 @@ struct TridiagonalMatrix {
     std::vector<float> lower_diag;
 };
 
+/// @brief Algorithm for solving tridiagonal linear systems with O(n) time complexity.
+/// @param A Tridiagonal matrix.
+/// @param rhs Right-hand-side vector.
+/// @return Solution vector to the linear system.
+std::vector<float> thomas_algorithm(const TridiagonalMatrix& A, const std::vector<float>& rhs) {
+
+    Decomposed LU_matrices = lu_decomposition(A.main_diag, A.upper_diag, A.lower_diag);
+
+    std::vector<float> lower_matrix = LU_matrices.lower;
+    std::vector<float> y = forward_substitution(lower_matrix, rhs); // Solution Vector Ly = rhs
+
+    std::vector<float> upper_matrix = LU_matrices.upper;
+    std::vector<float> x = backward_substitution(upper_matrix, A.upper_diag, y); // Solution Vector Ux = y
+
+    return x;
+}
+
+template <typename T>
+void print_vector(const std::vector<T>& vec) {
+    size_t n = vec.size();
+    for (size_t i=0; i<n; i++) {
+        std::cout << std::fixed << std::setprecision(2) << vec[i] << std::endl;
+    }
+    
+}
+
 int main() {
 
     // A = [1 1 0]
@@ -21,17 +47,10 @@ int main() {
     // Right-hand side
     std::vector<float> rhs = {6.0, 9.0, 6.0};
 
-    Decomposed LU_matrices = lu_decomposition(A.main_diag, A.upper_diag, A.lower_diag);
+    // Solution Vector
+    std::vector<float> x = thomas_algorithm(A, rhs);
 
-    std::vector<float> lower_matrix = LU_matrices.lower;
-    std::vector<float> y = forward_substitution(lower_matrix, rhs); // Solution Vector Ly = rhs
-
-    std::vector<float> upper_matrix = LU_matrices.upper;
-    std::vector<float> x = backward_substitution(upper_matrix, A.upper_diag, y); // Solution Vector Ux = y
-
-    std::cout << std::fixed << std::setprecision(2) << x[0] << std::endl;
-    std::cout << std::fixed << std::setprecision(2) << x[1] << std::endl;
-    std::cout << std::fixed << std::setprecision(2) << x[2] << std::endl;
+    print_vector(x);
 
     return 0;
 }
